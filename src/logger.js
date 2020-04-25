e8f4381520a21f354576bdf3aeaf8eb9ca908519
Alex Larrabee <nekotree556@gmail.com>
2020-04-24 23:54:19 -0700

    updated local server with bearer token

--- src/logger.js
+++ src/logger.js
@@ -5,7 +5,7 @@ const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
     transports: [
-        new winston.transports.File({ filename: '../info.log' })
+        new winston.transports.File({ filename: 'info.log' })
     ]
 })
 
